3c67eb3a5158de8b604e7ac0c63a7970524ffdc0
Rey <reneypassos9@gmail.com>
2026-05-12 16:37:34 -0300

    alteração de teste

--- src/main/java/com/reney/models/application/Program.java
+++ src/main/java/com/reney/models/application/Program.java
@@ -1,7 +1,9 @@
 package com.reney.models.application;
 
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
     }
 }
