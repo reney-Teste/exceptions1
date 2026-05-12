f0b2ba9497682486950f554d66c529300a697ac7
Rey <reneypassos9@gmail.com>
2026-05-12 20:40:32 -0300

    versão ruim

--- src/main/java/com/reney/application/Program.java
+++ src/main/java/com/reney/application/Program.java
@@ -1,7 +1,43 @@
 package com.reney.application;
 
+import com.reney.models.entities.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+
+        LocalDateTime checkIn = LocalDate.parse("20/03/2000", Reservation.dtf).atStartOfDay();
+        LocalDateTime checkOut = LocalDate.parse("25/03/2000", Reservation.dtf).atStartOfDay();
+        if (checkIn.isAfter(checkOut)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(100, checkIn, checkOut);
+            System.out.println(reservation);
+
+
+            System.out.println("\nEnter new checkIn date:");
+            checkIn = LocalDate.parse(sc.nextLine(), Reservation.dtf).atStartOfDay();
+            System.out.println("\nEnter new checkOut date:");
+            checkOut = LocalDate.parse(sc.nextLine(), Reservation.dtf).atStartOfDay();
+
+            LocalDateTime dateNow = LocalDateTime.now();
+
+            if (checkIn.isBefore(dateNow)) {
+                System.out.println("Enter a valid date");
+            } else {
+
+                reservation.updateDates(checkIn, checkOut);
+
+                System.out.println();
+                System.out.println(reservation);
+
+                sc.close();
+            }
+        }
     }
 }
--- src/main/java/com/reney/models/entities/Reservation.java
+++ src/main/java/com/reney/models/entities/Reservation.java
@@ -1,4 +1,57 @@
 package com.reney.models.entities;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Reservation {
+    private Integer roomNumber;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+
+    public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(){
+    }
+
+    public Reservation(Integer roomNumber, LocalDateTime checkIn, LocalDateTime checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        Duration duration = Duration.between(checkIn, checkOut);
+        return duration.toDays();
+    }
+
+    public void updateDates(LocalDateTime checkIn, LocalDateTime checkOut){
+        if (checkIn.isBefore(checkOut)) {
+            this.checkIn = checkIn;
+            this.checkOut = checkOut;
+        } else {
+            System.out.println("Invalid date!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Reservation: Room %d, check-in: %s, check-out: %s, %d nights", roomNumber, dtf.format(checkIn), dtf.format(checkOut), duration());
+    }
 }
